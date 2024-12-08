20a3aa9c0dfc7deb65fc8d37908811f8f1922678
vdvvdh <anxin.xie@student.rocva.nl>
2024-12-08 21:40:20 +0100

    All other games
    Copy pasted the first game with the other 4 same HTML CSS JS

--- index.js
+++ index.js
@@ -11,4 +11,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 sr.reveal('.eldenring-text', {delay: 200, origin: 'top'});
 sr.reveal('.eldenring-img', {delay: 450, origin: 'top'});
+sr.reveal('.dmc-text', {delay: 200, origin: 'top'});
+sr.reveal('.dmc-img', {delay: 450, origin: 'top'});
+sr.reveal('.nier-text', {delay: 200, origin: 'top'});
+sr.reveal('.nier-img', {delay: 450, origin: 'top'});
+sr.reveal('.ninesols-text', {delay: 200, origin: 'top'});
+sr.reveal('.ninesols-img', {delay: 450, origin: 'top'});
+sr.reveal('.darksouls-text', {delay: 200, origin: 'top'});
+sr.reveal('.darksouls-img', {delay: 450, origin: 'top'});
+
 
