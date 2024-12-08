bcb9cbd111b19ea07fa37a3e6a536ada9c6a9b87
vdvvdh <anxin.xie@student.rocva.nl>
2024-12-08 21:10:23 +0100

    JS html
    Change, moved game1js to index

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Document loaded');
   });
+
+  const sr = ScrollReveal({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.eldenring-text', {delay: 200, origin: 'top'});
+sr.reveal('.eldenring-img', {delay: 450, origin: 'top'});
+
